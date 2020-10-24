d37c26a8b125bc57f71d5a7671844b39f3d76604
Anton Todorov <shadowtabris84@gmail.com>
2020-10-24 17:09:32 +0300

    client authentication

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -2,12 +2,17 @@ import { Request, Response, NextFunction } from 'express';
 
 import { CustomError } from '../errors/custom-error';
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({
-    message: [{ message: 'Something went wrong' }],
+    errors: [{ message: 'Something went wrong' }],
   });
 };
---
+++ client/api/build-client.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const buildClient = ({ req }) => {
+  if (typeof window === 'undefined') {
+    return axios.create({
+      baseURL:
+        'http://my-release-ingress-nginx-controller.default.svc.cluster.local',
+      headers: req.headers,
+    });
+  } else {
+    return axios.create({
+      baseURL: '/',
+    });
+  }
+};
+
+export default buildClient;
