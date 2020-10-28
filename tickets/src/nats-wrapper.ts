e6ade6a2040fcd2c787f9facf03c24fa3611cfbb
Anton Todorov <shadowtabris84@gmail.com>
2020-10-28 11:19:32 +0200

    update event flow

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -13,6 +13,7 @@ class NatsWrapper {
 
   connect(clusterId: string, clientId: string, url: string) {
     this._client = nats.connect(clusterId, clientId, { url });
+    console.log('Connecting...');
 
     return new Promise((resolve, reject) => {
       this.client.on('connect', () => {
